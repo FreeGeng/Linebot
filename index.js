8f1a62799514949d116a5f696c742ae3132fc194
FreeGeng <nick830107@gmail.com>
2017-06-23 13:39:53 +0800

    fix index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var bot = linebot({
   channelId: 1521338926,
-  channelSecret: e90fe6f72c4165bcfaaab996c8158f54,
-  channelAccessToken: xBMq2+BJ/RtP6FvPKF0GjpJSVCTFQQYWFMSX8EJJAe0UfSDDcLqkxU9oGWFh1hJSc2QhapVx215C74mpzfPfRWRfw05p7MGKa/znmFrAs9L7AKTApVLIRLebkZQXCTiT8gC/d7AXJFt5zOzH7Sjj5gdB04t89/1O/w1cDnyilFU=
+  channelSecret: 'e90fe6f72c4165bcfaaab996c8158f54',
+  channelAccessToken: 'xBMq2+BJ/RtP6FvPKF0GjpJSVCTFQQYWFMSX8EJJAe0UfSDDcLqkxU9oGWFh1hJSc2QhapVx215C74mpzfPfRWRfw05p7MGKa/znmFrAs9L7AKTApVLIRLebkZQXCTiT8gC/d7AXJFt5zOzH7Sjj5gdB04t89/1O/w1cDnyilFU='
 });
 
