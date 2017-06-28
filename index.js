5d63469021ffcf01632f11eaaaa9e1dc6ee03f50
FreeGeng <nick830107@gmail.com>
2017-06-28 14:59:29 +0800

    add key

--- index.js
+++ index.js
@@ -176,7 +176,9 @@ function getExchangeRate(event) {
 
 function weather(event) {
   console.log('enter Weather function');
-  var city = ["臺北市", "新北市", "桃園市", "臺中市", "臺南市", "高雄市"];
+  var city = ["臺北市", "新北市", "桃園市", "臺中市", "臺南市", "高雄市","基隆市","新竹縣",
+  "新竹市","苗栗縣","彰化縣","南投縣","雲林縣","嘉義縣","嘉義市","屏東縣","宜蘭縣","花蓮縣",
+  "臺東縣","澎湖縣","金門縣","連江縣"];
   request({
     url: 'http://opendata.cwb.gov.tw/opendataapi?dataid=F-C0032-001&authorizationkey=CWB-984F180A-599F-4146-82BA-A276190F682B',
     method: 'GET',
@@ -209,7 +211,7 @@ function weather(event) {
 
       var weatherType = [];
 
-	  $('locationName').each(function(i, elem) {
+	  $('parameterValue').each(function(i, elem) {
       weatherType[i] = $(this).text();
       });
       weatherType.join(',');
